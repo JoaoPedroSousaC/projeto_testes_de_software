98b09f21283189b6922f13c29755bbad04a89f17
Isaac Antônio <isaacasb16@gmail.com>
2022-11-18 21:25:52 -0300

    Sistema_modificação

--- src/main/java/Sistema.java
+++ src/main/java/Sistema.java
@@ -1,49 +1,54 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.Math;
 
 public class Sistema implements FuncionalidadesIF {
 
-  //Falta restringir o cadastro para verificar se está sendo passado dados permitidos
+  // Falta restringir o cadastro para verificar se está sendo passado dados
+  // permitidos
   public Usuario cadastraUsuario(String nome, String email, String senha) throws Exception {
-    Usuario usuario = new Usuario(nome,email,senha);
+    Usuario usuario = new Usuario(nome, email, senha);
     return usuario;
   }
 
-  public Raizes calculaFuncaoSegundoGrau(double a, double b, double c){
-    Raizes raizes = new Raizes(a,b,c);
+  public Raizes calculaFuncaoSegundoGrau(double a, double b, double c) {
+    Raizes raizes = new Raizes(a, b, c);
     System.out.println(raizes.getRaizes());
     return raizes;
   }
 
-  public boolean ehTriangulo(double angulo1, double angulo2, double angulo3){
+  public boolean ehTriangulo(double angulo1, double angulo2, double angulo3) {
     double somaDosAngulos = angulo1 + angulo2 + angulo3;
-    double somaAngulosInternos = angulo1 + angulo2 + angulo3;
-    if(somaDosAngulos != 180) return false;
+    if (somaDosAngulos != 180)
+      return false;
     return true;
   }
 
-  public boolean ehTrianguloRetangulo(double lado1, double lado2, double lado3){
+  public boolean ehTrianguloRetangulo(double lado1, double lado2, double lado3) {
 
-    if(((lado1 == 90)||(lado2 == 90) || (lado3 == 90)) && ehTriangulo(lado1,lado2,lado3)) return true;
+    if ((Math.pow(lado1, 2) + Math.pow(lado2, 2) == Math.pow(lado3, 2))
+        || (Math.pow(lado3, 2) + Math.pow(lado2, 2) == Math.pow(lado1, 2))
+        || (Math.pow(lado1, 2) + Math.pow(lado3, 2) == Math.pow(lado2, 2)))
+      return true;
     return false;
   }
 
-
-  //FALTA FAZER -------------------------
-  public boolean ehRetangulo(double lado1, double lado2, double lado3, double lado4){
+  public boolean ehRetangulo(double lado1, double lado2, double lado3, double lado4) {
     List<Double> lados = new ArrayList<>();
     lados.add(lado1);
     lados.add(lado2);
     lados.add(lado3);
     lados.add(lado4);
     double var = lados.get(2);
-    int contador = 0, contadorRepeticoes =0;
+    int contador = 0, contadorRepeticoes = 0;
 
-    while(true){
-      if(lados.size()==0) break;
-      for(int i=1;i<lados.size();i++){
-        if(lados.get(0).equals(0) || lados.get(i).equals(0)) return false;
-        if(lados.get(0).equals(lados.get(i))){
+    while (true) {
+      if (lados.size() == 0)
+        break;
+      for (int i = 1; i < lados.size(); i++) {
+        if (lados.get(0).equals(0) || lados.get(i).equals(0))
+          return false;
+        if (lados.get(0).equals(lados.get(i))) {
           lados.remove(i);
           lados.remove(0);
           contador++;
@@ -51,39 +56,40 @@ public class Sistema implements FuncionalidadesIF {
         }
         contadorRepeticoes++;
       }
-      if(contadorRepeticoes>4) break;
+      if (contadorRepeticoes > 4)
+        break;
     }
-    if(contador == 2) return true;
+    if (contador == 2)
+      return true;
     return false;
   }
 
-
   public String classificaTriangulo(double angulo1, double angulo2, double angulo3) {
-    if(ehTriangulo(angulo1,angulo2,angulo3)){
-      double[] arrayAngulos = {angulo1,angulo2,angulo3};
-      if(ehTrianguloRetangulo(angulo1,angulo2,angulo3)) return "É um Triângulo Retângulo";
-      if((angulo1 > 90)||(angulo2 > 90) || (angulo3 > 90)) return "É um Triângulo Obtusângulo";
-      else return "É um Triângulo Acutângulo";
-    }
-    else return "Os Ângulos informados, não formam um triângulo!";
+    if (ehTriangulo(angulo1, angulo2, angulo3)) {
+      if (ehTrianguloRetangulo(angulo1, angulo2, angulo3))
+        return "É um Triângulo Retângulo";
+      if ((angulo1 > 90) || (angulo2 > 90) || (angulo3 > 90))
+        return "É um Triângulo Obtusângulo";
+      else
+        return "É um Triângulo Acutângulo";
+    } else
+      return "Os Ângulos informados, não formam um triângulo!";
   }
 
-
-  public double perimetroRetangulo(double lado1, double lado2, double lado3, double lado4){
-    if(ehRetangulo(lado1,lado2,lado3,lado4)){
+  public double perimetroRetangulo(double lado1, double lado2, double lado3, double lado4) {
+    if (ehRetangulo(lado1, lado2, lado3, lado4)) {
       double perimetro = lado1 + lado2 + lado3 + lado4;
       return perimetro;
     }
     return 0;
   }
 
-
-  public double areaRetangulo(double lado1, double lado2, double lado3, double lado4){
-    if(ehRetangulo(lado1,lado2,lado3,lado4)){
-      double[] arrayLados = {lado1,lado2,lado3,lado4};
-      double area=0;
-      for(int i=1;i<4;i++){
-        if(lado1 != arrayLados[i]) {
+  public double areaRetangulo(double lado1, double lado2, double lado3, double lado4) {
+    if (ehRetangulo(lado1, lado2, lado3, lado4)) {
+      double[] arrayLados = { lado1, lado2, lado3, lado4 };
+      double area = 0;
+      for (int i = 1; i < 4; i++) {
+        if (lado1 != arrayLados[i]) {
           area = lado1 * arrayLados[i];
           break;
         }
@@ -93,28 +99,28 @@ public class Sistema implements FuncionalidadesIF {
     return 0;
   }
 
-  //FALTA FAZER CALCULADORA ---------------------------------
-  public double calculadora(String expressao){return 0;}
+  // FALTA FAZER CALCULADORA ---------------------------------
+  public double calculadora(String expressao) {
+    return 0;
+  }
 
-
-  public double areaCirculo(double raio){
+  public double areaCirculo(double raio) {
     double pi = 3.14;
-    double area = pi * Math.pow(raio,2);
+    double area = pi * Math.pow(raio, 2);
     return area;
   }
 
-  public double areaPerimetro(double raio){
+  public double areaPerimetro(double raio) {
     double pi = 3.14;
     double perimetro = 2 * pi * raio;
     return perimetro;
   }
 
-  public double distanciaEntreDoisPontos(double x1, double y1, double x2, double y2){
-    double diferençaX = Math.pow((x2 - x1),2);
-    double diferençaY = Math.pow((y2 - y1),2);
-    double distancia = Math.sqrt(diferençaX+diferençaY);
+  public double distanciaEntreDoisPontos(double x1, double y1, double x2, double y2) {
+    double diferençaX = Math.pow((x2 - x1), 2);
+    double diferençaY = Math.pow((y2 - y1), 2);
+    double distancia = Math.sqrt(diferençaX + diferençaY);
     return distancia;
   }
-
 
 }
